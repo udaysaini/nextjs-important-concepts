619a39275a010be20e18af8f6831fabaf085454b
Uday Saini <uday10saini@gmail.com>
2024-07-07 14:25:26 -0400

    commit base nextjs setup, removed boiler plate

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-dvh flex justify-center items-center ">
+      <h1 className="text-4xl font-bold">Blank Screen. Nothin to see here :P</h1>
+    </main>
+  );
+}
